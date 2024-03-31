73b63e024ed2ef78fa96653da5b90dc780d4b137
PavloPushak <pashapushak1996@gmail.com>
2024-03-31 18:41:12 +0300

    feat: added dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'react-hooks'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/no-unstable-nested-components': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+      },
+    },
+  ],
 };
